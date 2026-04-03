68f47cd8b58df84be5f0bd9d6ac5b4043ea20b4e
Harshangi0110 <harshangi.polara@gmail.com>
2026-04-03 11:22:45 +0530

    Updated auth and todo logic

--- backend/src/controller/todos-controller.js
+++ backend/src/controller/todos-controller.js
@@ -2,7 +2,7 @@ const todoService = require("../service/todos-services");
 
 create = async (req, res) => {
   try {
-    const todo = await todoService.crea+teTodo(req.body, req.user.id);
+    const todo = await todoService.createTodo(req.body, req.user.id);
     res.status(201).json(todo);
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -16,7 +16,11 @@ getAll = async (req, res) => {
 
 update = async (req, res) => {
   try {
-    const todo = await todoService.updateTodo(req.params.id, req.body.title, req.user.id);
+    const todo = await todoService.updateTodo(
+      req.params.id,
+      req.body.title,
+      req.user.id,
+    );
     res.json(todo);
   } catch (err) {
     res.status(404).json({ message: err.message });
